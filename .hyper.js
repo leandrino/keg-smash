9261af884b8d18cfd4c5c9fe3456f398c4fd6716
Leandro Alexandrino <leandro@leandrino.me>
2017-03-04 21:52:36 -0300

    add hyperterm config

---
+++ .hyper.js
@@ -0,0 +1,97 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 12,
+
+    // font family with optional fallbacks
+    fontFamily: 'Fira Code, Monaco, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: '#32b67a',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+
+    // color of the text
+    foregroundColor: '#fff',
+
+    // terminal background color
+    backgroundColor: '#1b1d1c',
+
+    // border color (window, tabs)
+    borderColor: '#32b67a',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '12px 12px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#1B1D1C',
+      red: '#EF3E4A',
+      green: '#32B67A',
+      yellow: '#F0CT61',
+      blue: '#0BBCD6',
+      magenta: '#CC00FF',
+      cyan: '#A6CFE2',
+      white: '#F9F7E8',
+      lightBlack: '#333',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff'
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+
+    hyperStatusLine: {
+      dirtyColor: 'salmon',
+      arrowsColor: 'ivory',
+      fontFamily: '"Fira Code"',
+    }
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  // plugins: ['hyperpower'],
+  plugins: ['hyper-statusline', 'hyper-nord', 'hyper-tabs-enhanced'],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+};
